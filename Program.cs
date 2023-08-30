a68321ad97d0609343a8270ecf40f29f7231a0fc
allanss0 <allanss@live.com>
2023-08-29 21:11:14 -0300

    Added SQL Server connection

--- Program.cs
+++ Program.cs
@@ -1,16 +1,22 @@
 using Webapiaspnet.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Migrations;
+using System;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("Webapiconnection");
 
-builder.Services.AddDbContext<WebapiContext>(opts =>
-    opts.UseMySql((connectionString), ServerVersion.AutoDetect(connectionString)));
-// Add services to the container.
+//connection to local My SQL
+/*builder.Services.AddDbContext<WebapiContext>(opts =>
+    opts.UseMySql((connectionString), ServerVersion.AutoDetect(connectionString)));*/
 
+//Connection using SQL Server
+builder.Services.AddDbContext<WebapiContext>(opts =>
+    opts.UseSqlServer(connectionString));
+
+// Add services to the container.
 builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
